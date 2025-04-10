69b02462ef81dc10e820209ee80f86a3096a4c5b
Kiengchay Gomez <kphoutthavong@gmail.com>
2025-04-10 11:23:34 -0500

    implement get_data and handle_data

--- chapter-11-the-web-and-search/case-study/collector.py
+++ chapter-11-the-web-and-search/case-study/collector.py
@@ -7,9 +7,14 @@ class Collector(HTMLParser):
 
     def __init__(self, url):
         'initializes parser, the url, and a list'
-        HTMLParser.__init__(self)
+        HTMLParser.__init__(self) #super().__init__()
         self.url = url
         self.links = []
+        self.data = []
+
+    # https://chatgpt.com/c/67f7ed89-4de4-800e-be78-0c3ea8c46af8
+    def handle_data(self, data):
+        self.data.append(data)
 
     def handle_starttag(self, tag, attrs):
         'collect hyperlink URLs in their absolute format'
@@ -21,6 +26,10 @@ class Collector(HTMLParser):
                     if absolute[:4] == 'http':
                         self.links.append(absolute)
 
+    def get_data(self):
+        return ''.join(self.data)
+    
     def get_links(self):
         'returns hyperlinks URLs in their absolute format'
         return self.links
+    
